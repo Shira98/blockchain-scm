e09f933a2fd9fbbf22e174676cbd4983779b4310
Shira98 <shirayukipr@gmail.com>
2022-05-15 02:24:03 +0530

    To trigger builds.

--- appfrontend/src/App.test.js
+++ appfrontend/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//Test class for App component.
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
