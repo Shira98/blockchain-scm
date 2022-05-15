726c94a65eaca55c7d809c35d575aee12a5c9b39
Shira98 <shirayukipr@gmail.com>
2022-05-15 23:05:29 +0530

    Resolved test failures.

--- test/TestSupplyChainLifecycle.sol
+++ test/TestSupplyChainLifecycle.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.4.21 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
@@ -6,25 +7,30 @@ import "../contracts/SupplyChainLifecycle.sol";
 
 contract TestSupplyChainLifecycle {
 
-    function testItStoresAValue() public {
+    function testItStoresTheProduct() public {
+
         SupplyChainLifecycle supplyChainLifecycle = SupplyChainLifecycle(DeployedAddresses.SupplyChainLifecycle());
 
         supplyChainLifecycle.produceProduct("Test Product","Test Desc", 12, 1, address(0));
 
-        // SupplyChainLifecycle.Product expected = SupplyChainLifecycle.Product({
-        //     productStatus: Status.PRODUCED,
-        //     currentStatusUser: address(0),
-        //     productId: 0,
-        //     productName: "Test Product",
-        //     productDesc: "Test Desc",
-        //     productPrice: 12,
-        //     productQuantity: 1,
-        //     producerAddress: address(0),
-        //     distributorAddress: address(0),
-        //     retailerAddresses: address(0),
-        //     consumerAddress: address(0)
-        // });
+        SupplyChainLifecycle.Product[] memory productDetailsArray = supplyChainLifecycle.getAllProductDetails();
 
-        Assert.equal(supplyChainLifecycle.getAllProductDetails(), 1, "A single product should have been returned.");
+        Assert.equal(productDetailsArray.length, 1, "One product should have been returned.");
+        Assert.equal(productDetailsArray[0].productQuantity, 1, "The quantity of the product stored should be 1.");
+        Assert.equal(productDetailsArray[0].productName, "Test Product", "The name of the product stored should be - Test Product.");
+        Assert.equal(productDetailsArray[0].productId, 0, "The ID of the product stored should be 0.");
+  
+    }
+
+    function testItReturnsProductDetailsByID() public {
+        SupplyChainLifecycle supplyChainLifecycle = SupplyChainLifecycle(DeployedAddresses.SupplyChainLifecycle());
+
+        supplyChainLifecycle.produceProduct("Test Product","Test Desc", 12, 1, address(0));
+
+        SupplyChainLifecycle.Product memory productDetails = supplyChainLifecycle.getProductDetails(0);
+
+        Assert.equal(productDetails.productQuantity, 1, "The quantity of the product stored should be 1.");
+        Assert.equal(productDetails.productName, "Test Product", "The name of the product stored should be - Test Product.");
+        Assert.equal(productDetails.productId, 0, "The ID of the product stored should be 0.");
     }
 }
