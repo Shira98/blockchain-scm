d476e7229032b273d4cf80b8914289115867181b
Shirayuki <32385163+Shira98@users.noreply.github.com>
2022-05-21 12:38:23 +0530

    Added image for 404 page.

--- appfrontend/src/components/static/NotFound.js
+++ appfrontend/src/components/static/NotFound.js
@@ -16,7 +16,7 @@ export default class NotFound extends React.Component {
     render() {
         return(
             <div className="not-found">
-                <h1>404</h1>
+                <img src="404.png" alt="404"/>
                 <h2>Oops! Looks like the page you're looking for doesn't exist.</h2>
                 <Grid container justifyContent="center">
                     <Grid item xs={2}>
